d93d5e230508f4a5093c1a5381cf7156d544665b
Ully Machado <ullyvieira@hotmail.com>
2025-05-08 00:09:19 -0300

    Rotas consertadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatButton, MatIcon],
+  imports: [RouterOutlet, RouterModule, MatToolbar, MatButton, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
