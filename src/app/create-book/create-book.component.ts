f027b34e04a0d3d14dc980244081cca80bfe6a88
Ully Machado <ullyvieira@hotmail.com>
2025-05-09 21:20:47 -0300

    Ajuste de estilo

--- src/app/create-book/create-book.component.ts
+++ src/app/create-book/create-book.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input'
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatCardModule} from '@angular/material/card'
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker'; // Adicione isso se ainda não estiver importado
 
 @Component({
   selector: 'app-create-book',
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule, MatCardModule, ReactiveFormsModule],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatDatepickerModule
+  ],
   templateUrl: './create-book.component.html',
   styleUrl: './create-book.component.scss',
 })
@@ -16,10 +25,19 @@ export class CreateBookComponent {
   form: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({});
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      autor: ['', Validators.required],
+      editora: ['', Validators.required],
+      isbn: ['', Validators.required],
+      dataLancamento: ['', Validators.required]
+    });
   }
 
-  onSubmit():void {
-
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log('Formulário enviado com os dados:', this.form.value);
+      // Aqui você pode fazer o POST ou lógica que quiser com os dados
+    }
   }
 }
